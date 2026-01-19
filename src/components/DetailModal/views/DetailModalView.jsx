53d878a78f7b11e0ab8b3c17f213320b0cfff3c7
Manas <manasagnihotri129@gmail.com>
2026-01-19 14:27:46 -0500

    Fix unused useState build error

--- src/components/DetailModal/views/DetailModalView.jsx
+++ src/components/DetailModal/views/DetailModalView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function DetailModalView({ isOpen, type, data, onClose }) {
   if (!isOpen || !data) return null;
